b1b367b71d983967a27a0770d802103237b34ef9
Marcos Willian <markusuuuu@gmail.com>
2020-09-01 03:50:10 -0300

    retomando projeto

--- appgobarber/src/pages/CreateAppointment/index.tsx
+++ appgobarber/src/pages/CreateAppointment/index.tsx
@@ -1,14 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import { useRoute } from '@react-navigation/native';
-import { Container } from './styles';
+import React, { useEffect, useState, useCallback } from 'react';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import api from '../../services/apiClient';
+
+import { Providers } from '../Dashboard/types';
+import { useAuth } from '../../hooks/auth';
+
+import {
+  Container,
+  Header,
+  HeaderTitleContainer,
+  BackButton,
+  HeaderTitleText,
+  ProfileButton,
+  ProfileImage,
+  NoImageProfile,
+} from './styles';
 
 interface RouteParams {
   providerId: string | undefined;
 }
 
 const CreateAppointment: React.FC = () => {
-  const { params } = useRoute();
   const [providerId, setProviderId] = useState('');
+  const [providers, setProviders] = useState<Providers[]>([]);
+
+  const { params } = useRoute();
+  const { user } = useAuth();
+  const { navigate, goBack } = useNavigation();
+
+  const navigateToProfile = useCallback(() => navigate('Profile'), [navigate]);
 
   useEffect(() => {
     const routeParams = params as RouteParams;
@@ -16,9 +38,44 @@ const CreateAppointment: React.FC = () => {
     if (routeParams.providerId) {
       setProviderId(routeParams.providerId);
     }
+
+    api.get('providers').then(response => {
+      const formattedProviders = response.data.map(
+        ({ id, avatar_url, name }: Providers) => ({
+          id,
+          avatar_url,
+          name,
+        }),
+      );
+
+      setProviders(formattedProviders);
+    });
   }, [params]);
 
-  return <Container />;
+  return (
+    <Container>
+      <Header>
+        <HeaderTitleContainer>
+          <BackButton onPress={() => goBack()}>
+            <Icon name="chevron-left" size={24} color="#999591" />
+          </BackButton>
+          <HeaderTitleText>Cabeleireiros</HeaderTitleText>
+        </HeaderTitleContainer>
+
+        {user.avatar_url ? (
+          <ProfileButton onPress={navigateToProfile}>
+            <ProfileImage source={{ uri: user.avatar_url }} />
+          </ProfileButton>
+        ) : (
+          <ProfileButton onPress={navigateToProfile}>
+            <NoImageProfile>
+              <Icon name="user" size={36} color="#ff9000" />
+            </NoImageProfile>
+          </ProfileButton>
+        )}
+      </Header>
+    </Container>
+  );
 };
 
 export default CreateAppointment;
--- appgobarber/src/pages/CreateAppointment/styles.ts
+++ appgobarber/src/pages/CreateAppointment/styles.ts
@@ -1,6 +1,51 @@
 import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
   background: blue;
+`;
+
+export const Header = styled.View`
+  padding: ${getStatusBarHeight()}px 24px 24px 24px;
+  background: #28262e;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderTitleContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const BackButton = styled.TouchableOpacity`
+  margin-right: 12px;
+  padding: 4px;
+`;
+
+export const HeaderTitleText = styled.Text`
+  color: #f4ede8;
+  font-size: 22px;
+  font-family: 'RobotoSlab-Regular';
+  line-height: 26px;
+`;
+
+export const ProfileButton = styled.TouchableOpacity``;
+
+export const ProfileImage = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 1000px;
+`;
+
+export const NoImageProfile = styled.View`
+  width: 56px;
+  height: 56px;
+  justify-content: center;
+  align-items: center;
+
+  border: 2px solid #ff9000;
+  border-radius: 1000px;
 `;
