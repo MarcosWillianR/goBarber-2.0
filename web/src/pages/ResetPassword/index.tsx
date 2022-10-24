7b349458777eed0611eb347caf71782536865ab3
Marcos Willian <markusuuuu@gmail.com>
2022-10-23 21:21:28 -0300

    chore: fix language and code

--- web/src/pages/ResetPassword/index.tsx
+++ web/src/pages/ResetPassword/index.tsx
@@ -31,70 +31,57 @@ const ResetPassword: React.FC = () => {
 
   const { addToast } = useToast();
 
-  const handleSubmit = useCallback(
-    async (data: ResetPasswordFormData) => {
-      try {
-        setLoading(true);
-        formRef.current?.setErrors({});
+  const handleSubmit = useCallback(async (data: ResetPasswordFormData) => {
+    try {
+      setLoading(true);
+      formRef.current?.setErrors({});
 
-        const schema = Yup.object().shape({
-          password: Yup.string().required('Senha obrigatória'),
-          password_confirmation: Yup.string().oneOf(
-            [Yup.ref('password'), null],
-            'Confirmação inválida',
-          ),
-        });
+      const schema = Yup.object().shape({
+        password: Yup.string().required('Senha obrigatória'),
+        password_confirmation: Yup.string().oneOf(
+          [Yup.ref('password'), null],
+          'Confirmação inválida',
+        ),
+      });
 
-        await schema.validate(data, { abortEarly: false });
+      await schema.validate(data, { abortEarly: false });
 
-        if (!locationParamsParsed?.token) {
-          throw new Error();
-        }
-
-        const { token } = locationParamsParsed;
-        const { password, password_confirmation } = data;
-
-        await api.post('/password/reset', {
-          token,
-          password,
-          password_confirmation,
-        });
-
-        addToast({
-          title: 'Sucesso!',
-          type: 'success',
-          description: 'Sua senha foi redefinida com sucesso.',
-        });
-
-        history.push('/');
-      } catch (err) {
-        if (err instanceof Yup.ValidationError) {
-          const errors = getValidationErrors(err);
-
-          formRef.current?.setErrors(errors);
-
-          return;
-        }
-
-        addToast({
-          title: 'Erro na Redefinição',
-          type: 'error',
-          description:
-            'Aconteceu um erro ao redefinir sua senha, tente novamente.',
-        });
-      } finally {
-        setLoading(false);
+      if (!locationParamsParsed?.token) {
+        throw new Error();
       }
-    },
-    [addToast, history, locationParamsParsed],
-  );
+
+      const { token } = locationParamsParsed;
+      const { password, password_confirmation } = data;
+
+      await api.post('/password/reset', {
+        token,
+        password,
+        password_confirmation,
+      });
+
+      addToast({
+        title: 'Sucesso!',
+        type: 'success',
+        description: 'Sua senha foi redefinida com sucesso.',
+      });
+
+      history.push('/');
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+
+        return;
+      }
+    } finally {
+      setLoading(false);
+    }
+  }, [addToast, history, locationParamsParsed]);
 
   useEffect(() => {
     const parser = UrlParser(location);
-
-    if (parser) {
-      setLocationParamsParsed(parser);
-    }
+    if (parser) setLocationParamsParsed(parser);
   }, [location]);
 
   return (
